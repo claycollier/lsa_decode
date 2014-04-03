9cb914f724f984a30ae20ffac4b65d54ec96622d
Clay Collier <clay.collier@gmail.com>
2014-04-03 00:34:17 -0400

    Linter fixes, catch interrupts cleanly

--- lsa_decode.py
+++ lsa_decode.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-import sys
 import argparse
 
 def main():
      parser = argparse.ArgumentParser(description='Decodes LSA secret data.')
      parser.add_argument('-f', "--infile", type=argparse.FileType('r'), 
-               metavar='filename', help="Specify the file to read", default='-')
+               metavar='filename', 
+               help="Specify the file to read. Defaults to stdin.", default='-')
      args = parser.parse_args()
 
      text = decode(args.infile)
@@ -15,15 +15,18 @@ def main():
 
 def decode(input_file):
      out = list()
-     for line in input_file:
-          chars = line.split()
-          for c in chars:
-               try:
-                    byte = int(c, 16)
-                    out.append(chr(byte))
-               except ValueError as e:
-                    print('Invalid input value: %s' % e)
-                    return ''
+     try:
+          for line in input_file:
+               chars = line.split()
+               for char in chars:
+                    try:
+                         byte = int(char, 16)
+                         out.append(chr(byte))
+                    except ValueError as err:
+                         print('Invalid input value: %s' % err)
+                         return ''
+     except KeyboardInterrupt:
+          return ''
 
      return ''.join(out)
 
